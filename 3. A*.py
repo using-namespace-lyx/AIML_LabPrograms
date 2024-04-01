ab05328848856690b2f9f6fb899c89e3c55ebd37
using-namespace-lyx <141064340+using-namespace-lyx@users.noreply.github.com>
2024-04-01 17:11:02 +0530

    Update 3. A*.py

--- 3. A*.py
+++ 3. A*.py
@@ -10,15 +10,15 @@ class PuzzleState:
         return False  # Define a default comparison method
 
 class Puzzle:
-    def __init__(self, start, goal):
-        self.start = start
-        self.goal = goal
+    def __init__(self, start_value, goal_value):
+        self.start_value = start_value
+        self.goal_value = goal_value
 
     def print_state(self, state):
         print(state[:, :])
 
     def is_goal(self, state):
-        return np.array_equal(state, self.goal)
+        return np.array_equal(state, self.goal_value)
 
     def get_possible_moves(self, state):
         possible_moves = []
@@ -33,11 +33,11 @@ class Puzzle:
         return possible_moves
 
     def heuristic(self, state):
-         return np.count_nonzero(state != self.goal)
+         return np.count_nonzero(state != self.goal_value)
 
     def solve(self):
         queue = PriorityQueue()
-        start_state = PuzzleState(self.start, None)
+        start_state = PuzzleState(self.start_value, None)
         queue.put((0, start_state))  # Put State object in queue
         visited = set()
 
@@ -55,16 +55,16 @@ class Puzzle:
 
 # Test the function
 print("Start matrix:")
-start_state = np.array([[int(x) for x in input().split()] for _ in range(3)])
+start_value = np.array([[int(x) for x in input().split()] for _ in range(3)])
 print("Goal matrix:")
-goal_state = np.array([[int(x) for x in input().split()] for _ in range(3)])
-puzzle = Puzzle(start_state, goal_state)
-solution = puzzle.solve()
-if solution is not None:
+goal_value = np.array([[int(x) for x in input().split()] for _ in range(3)])
+puzzle = Puzzle(start_value, goal_value)
+solution_state = puzzle.solve()
+if solution_state is not None:
     moves = []
-    while solution is not None:  # Go through parents to get moves
-        moves.append(solution.current_value)
-        solution = solution.parent_state
+    while solution_state is not None:  # Go through parents to get moves
+        moves.append(solution_state.current_value)
+        solution_state = solution_state.parent_state
     for move in reversed(moves):  # Print moves in correct order
         puzzle.print_state(move)
 else:
